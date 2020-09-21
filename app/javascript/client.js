0c3dfa1c62489599f86f297c9a38fc66ec8446f8
Mohamed Hamed <hamed.ccp@gmail.com>
2020-09-21 10:54:02 +0200

    Optimize cache utilization :rocket:
    By defining the cache key for the Recipe type, we can allow the
    Recipe to be read from the cache directly without needing to hit
    the network!

--- app/javascript/client.js
+++ app/javascript/client.js
@@ -4,9 +4,24 @@ const csrfToken = document
   .querySelector('meta[name=csrf-token]')
   .getAttribute('content');
 
+const cache = new InMemoryCache({
+  typePolicies: {
+    Query: {
+      fields: {
+        recipe(_, { args, toReference }) {
+          return toReference({
+            __typename: 'Recipe',
+            id: args.id,
+          });
+        },
+      },
+    },
+  },
+});
+
 const client = new ApolloClient({
   uri: '/graphql',
-  cache: new InMemoryCache(),
+  cache,
   credentials: 'same-origin',
   headers: {
     'X-CSRF-Token': csrfToken,
