9cdc3d9438143257e177b29fd255da3f73593482
Mohamed Hamed <hamed.ccp@gmail.com>
2020-09-21 03:52:25 +0200

    Load graphql queries by a graphql-loader

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,3 +1,11 @@
-const { environment } = require('@rails/webpacker')
+const { environment } = require('@rails/webpacker');
 
-module.exports = environment
+const gqlLoader = {
+  test: /\.(graphql|gql)$/,
+  exclude: /node_modules/,
+  loader: 'graphql-tag/loader',
+};
+
+environment.loaders.append('graphql', gqlLoader);
+
+module.exports = environment;
