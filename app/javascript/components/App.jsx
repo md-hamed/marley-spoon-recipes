c430757d103dd5ae9b175249c40bbbc9688b2c91
Mohamed Hamed <hamed.ccp@gmail.com>
2020-09-21 03:54:01 +0200

    Modify react routes to reflect the new components structure

--- app/javascript/components/App.jsx
+++ app/javascript/components/App.jsx
@@ -1,27 +1,32 @@
 import React from 'react';
+import 'semantic-ui-css/semantic.min.css';
+import { ApolloProvider } from '@apollo/client';
 import {
   BrowserRouter as Router,
   Route,
   Switch,
   Redirect,
 } from 'react-router-dom';
+import client from '../client';
 import Recipes from './Recipes';
 import Recipe from './Recipe';
 
 const App = () => (
-  <Router>
-    <Switch>
-      <Route exact path="/">
-        <Redirect to="/recipes" />
-      </Route>
-      <Route exact path="/recipes" component={Recipes} />
-      <Route
-        exact
-        path="/recipe"
-        component={Recipe}
-      />
-    </Switch>
-  </Router>
+  <ApolloProvider client={client}>
+    <Router>
+      <Switch>
+        <Route exact path="/">
+          <Redirect to="/recipes" />
+        </Route>
+        <Route exact path="/recipes" component={Recipes} />
+        <Route
+          exact
+          path="/recipe"
+          component={Recipe}
+        />
+      </Switch>
+    </Router>
+  </ApolloProvider>
 );
 
 export default App;
